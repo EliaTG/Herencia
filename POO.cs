5d3ac6d5330ac05d4c661539fb0f74fd58271efe
EliaTG <71838009+EliaTG@users.noreply.github.com>
2020-11-24 13:01:46 -0400

    Create POO.cs

---
+++ POO.cs
@@ -0,0 +1,38 @@
+
+
+
+// 3 Crear tres clases ClaseA, ClaseB, ClaseC que ClaseB herede de ClaseA y ClaseC herede de ClaseB.
+//  Definir un constructor a cada clase que muestre un mensaje. Luego definir un objeto de la clase ClaseC.
+/*
+namespace POO
+{
+    class A
+    {
+      public  A(){
+        Console.WriteLine(" Hola soy A");
+      }
+       
+    }
+    class B : A
+    {
+     public  B(){
+      Console.WriteLine(" Hola soy B");
+     }
+    }
+    class C : B
+    {
+     public  C(){
+       Console.WriteLine(" Hola soy C");
+      }
+    }
+   class POO
+   {
+    static void Main(string[] args)
+    {
+     B obj1 = new B();
+     Console.ReadKey();
+    }  
+   }
+  
+}
+*/
